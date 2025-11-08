ef6817cc6da707804119e29bbddfab6352bd41b2
Stiven-023 <stiven.castro@correounivalle.edu.co>
2025-11-08 00:36:18 -0500

    structure correction in activity_main

--- app/src/main/java/com/univalle/equipotres/LoginFragment.kt
+++ app/src/main/java/com/univalle/equipotres/LoginFragment.kt
@@ -29,6 +29,7 @@ class LoginFragment : Fragment() {
         }
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
