43ca9c3568e2ef40f138015a055cc4e2d07d638f
MarvinB <marvin.nunez@correounivalle.edu.co>
2025-12-10 22:02:30 -0500

    update

--- app/src/main/java/com/univalle/equipotres/di/FirebaseModule.kt
+++ app/src/main/java/com/univalle/equipotres/di/FirebaseModule.kt
@@ -1,15 +1,24 @@
 package com.univalle.equipotres.di
 
+import android.content.Context
 import com.google.firebase.firestore.FirebaseFirestore
+import com.univalle.equipotres.utils.SessionManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object FirebaseModule {
+
+    @Provides
+    @Singleton
+    fun provideSessionManager(@ApplicationContext context: Context): SessionManager {
+        return SessionManager(context)
+    }
 
     @Provides
     @Singleton
